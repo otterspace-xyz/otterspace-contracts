4eb59eb4752c2eadc9a50319bae98fac1025e34f
Rahul Rumalla <rahul.rumalla@soundcloud.com>
2022-03-16 16:11:20 +0100

    wip checkpoint

--- contracts/OtterBadge.sol
+++ contracts/OtterBadge.sol
@@ -7,19 +7,45 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract OtterBadge is ERC721URIStorage {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    Counters.Counter private nextBadgeId;
 
-    constructor() ERC721("OtterSpace Badge", "OTTER") {}
+    event BadgeMinted(
+        address from,
+        address to,
+        uint256 tokenId,
+        string tokenURI,
+        string name,
+        string group,
+        uint8 level
+    );
 
-    function mintBadge(address to, string memory tokenURI)
-        public
-        returns (uint256)
-    {
-        _tokenIds.increment();
+    constructor() ERC721("Otterspace Badge", "OTTER") {}
 
-        uint256 tokenId = _tokenIds.current();
-        _safeMint(to, tokenId);
+    // Issues: anyone can mint even an attacker, tokenURI can be arbitrary including name, group & level params
+    // Could mind a "Badge Collection" and ensure that collectionId exists
+    // How do we restrict bad actors from minting? whitelisting?
+    function mintBadge(
+        address receiver,
+        string memory tokenURI,
+        string memory name,
+        string memory group,
+        uint8 level
+    ) public returns (uint256) {
+        nextBadgeId.increment();
+        uint256 tokenId = nextBadgeId.current();
+
+        _safeMint(receiver, tokenId);
         _setTokenURI(tokenId, tokenURI);
+
+        emit BadgeMinted(
+            msg.sender,
+            receiver,
+            tokenId,
+            tokenURI,
+            name,
+            group,
+            level
+        );
 
         return tokenId;
     }
