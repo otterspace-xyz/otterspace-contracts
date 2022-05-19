73bfb95acb2a201d7bc360cddd770b610bfdaa4e
Tim Daubenschütz <tim@daubenschuetz.de>
2022-05-19 18:26:23 +0200

    Facotorize Badges collection with Vendor ownership
    - Fixes #14

--- src/BadgeVendor.sol
+++ src/BadgeVendor.sol
@@ -1,13 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.6;
 
+import { Badges } from "./Badges.sol";
 import { ChainClaim } from "chain-claim/ChainClaim.sol";
 
+struct Vendor {
+  address issuer;
+  string name;
+}
+
+struct Collection {
+  string name;
+  string symbol;
+}
+
 contract BadgeVendor is ChainClaim {
+  Badges public badges;
   constructor(
-    address issuer,
-    string memory name
-  ) ChainClaim(issuer, name) {}
-
-
+    Vendor memory v,
+    Collection memory c
+  ) ChainClaim(v.issuer, v.name) {
+    badges = new Badges(c.name, c.symbol, address(this));
+  }
 }
--- src/BadgeVendor.t.sol
+++ src/BadgeVendor.t.sol
@@ -2,18 +2,27 @@
 pragma solidity ^0.8.6;
 
 import {Test} from "forge-std/Test.sol";
-import {BadgeVendor} from "./BadgeVendor.sol";
+import {BadgeVendor, Vendor, Collection} from "./BadgeVendor.sol";
 
 contract BadgeVendorTest is Test {
   BadgeVendor bv;
 
   function setUp() public {
     address issuer = address(this);
-    string memory name = "name";
-    bv = new BadgeVendor(issuer, name);
+    Vendor memory vendor = Vendor(issuer, "name");
+
+    string memory name = "Name";
+    string memory symbol = "Symbol";
+    Collection memory coll = Collection(name, symbol);
+
+    bv = new BadgeVendor(vendor, coll);
   }
 
-  function testParameters() public {
+  function testChainClaim() public {
     assertEq(bv.ISSUER(), address(this));
+  }
+
+  function testCollectionOwnership() public {
+    assertEq(bv.badges().owner(), address(bv));
   }
 }
