3ad45620fcc0defe8ffa781dcefb42f92b9a197b
Tim Daubenschütz <tim@daubenschuetz.de>
2022-05-19 17:40:04 +0200

    Implement `tokenId` increment with OZ `Counter`
    - Fixes #11

--- src/Badges.sol
+++ src/Badges.sol
@@ -1,19 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.6;
 
-import { ERC4973 } from "ERC4973/ERC4973.sol";
+import {ERC4973} from "ERC4973/ERC4973.sol";
+import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
 
 string constant uri = "https://ipfs.io/ipfs/QmdoUaYzKCMUmeH473amYJNyFrL1a6gtccQ5rYsqqeHBsC";
 
 contract Badges is ERC4973 {
-  uint256 tokenId;
-  constructor() ERC4973("Otterspace Badges", "OTTER") {}
+	using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor(
+    string memory name,
+    string memory symbol
+  ) ERC4973(name, symbol) {}
+
   function mint(
     address to
   ) external returns (uint256) {
-    super._mint(to, tokenId, uri);
-    tokenId++;
-    return tokenId-1;
+    uint256 newTokenId = _tokenIds.current();
+		_mint(to, newTokenId, uri);
+    _tokenIds.increment();
+    return newTokenId;
   }
-
 }
--- src/Badges.t.sol
+++ src/Badges.t.sol
@@ -2,25 +2,36 @@
 pragma solidity ^0.8.6;
 
 import {DSTest} from "ds-test/test.sol";
-import {Badges} from "./Badges.sol";
+import {Badges, uri} from "./Badges.sol";
+
+string constant name = "Name";
+string constant symbol = "Symbol";
 
 contract BadgesTest is DSTest {
   Badges b;
 
   function setUp() public {
-    b = new Badges();
+    b = new Badges(name, symbol);
+  }
+
+  function testNameAndSymbol() public {
+    assertEq(b.name(), name);
+    assertEq(b.symbol(), symbol);
   }
 
   function testMinting() public {
     address to = address(1);
     uint256 tokenId = b.mint(to);
     assertEq(tokenId, 0);
+    assertEq(b.tokenURI(tokenId), uri);
   }
   function testMintingTwice() public {
     address to = address(1);
     uint256 tokenId = b.mint(to);
     assertEq(tokenId, 0);
+    assertEq(b.tokenURI(tokenId), uri);
     uint256 tokenId1 = b.mint(to);
     assertEq(tokenId1, 1);
+    assertEq(b.tokenURI(tokenId1), uri);
   }
 }
