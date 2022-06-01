0f743262f5cecf03936912b06a377b9fa262b32a
Tim Daubenschütz <tim@daubenschuetz.de>
2022-06-01 17:16:40 +0200

    Add custom linter for console log statements
    - Fixes #26

---
+++ scripts/custom-linter.sh
@@ -0,0 +1,7 @@
+if grep -rlq "forge-std/console" . --exclude-dir=lib --exclude-dir=node_modules; then
+  echo "found line that breaks linter rule"
+  grep -rl "forge-std/console" .. --exclude-dir=lib --exclude-dir=node_modules -exclude-dir=scripts
+  exit 1
+else
+  exit 0
+fi
