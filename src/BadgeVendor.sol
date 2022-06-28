f77e251027dd4ebe424510921d3895d096e888a5
Rahul Rumalla <rahulrumalla@gmail.com>
2022-06-28 10:15:20 +0200

    Reducing scope of this to just replacing chain-claim with ERC4973Permit

--- src/BadgeVendor.sol
+++ src/BadgeVendor.sol
@@ -1,49 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.6;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Badges} from "./Badges.sol";
 
-// rename to IssuerRegistry.sol? or a better name?
 contract BadgeVendor {
-  // a badge spec is a JSON blob representing the design of the badge to be minted and also uploaded to IPFS
-  // a badge spec is fully unique and non-fungible - should THIS be a $ROMP token instead?
-  mapping(string => uint256) private _badgeSpecOwners;
-
-  ERC721 _romp; // is there a better way?
-  Badges _badges; // is there a better way?
-
-  // necessary?
-  event BadgeSpecCreated(uint256 indexed rompTokenId, address owner, string tokenURI);
-
-  // necessary?
-  event BadgeMinted(address indexed owner, uint256 indexed tokenId, string tokenURI);
+  Badges _badges;
 
   constructor (
-    address rompTokenAddress,
     address badgesAddress
   ) {
-    _romp = ERC721(rompTokenAddress);
     _badges = Badges(badgesAddress);
-  }
-
-  // creates a spec and create an association of a $ROMP to specs created
-  function createBadgeSpec(
-    uint256 tokenId,
-    string calldata tokenURI
-  ) external {
-    // authorize that only $ROMP token holder can create new specs
-    require(msg.sender == _romp.ownerOf(tokenId), "createBadgeSpec: unauthorized");
-
-    // register the spec owner
-    _badgeSpecOwners[tokenURI] = tokenId;
-
-    // emit event that badge spec was created, indexed over romp token ID
-    emit BadgeSpecCreated(tokenId, msg.sender, tokenURI);
-  }
-
-  function ownerOfSpec(string calldata tokenUri) public view returns (uint256) {
-    return _badgeSpecOwners[tokenUri];
   }
 
   function claimBadge(
@@ -53,34 +19,10 @@ contract BadgeVendor {
     bytes32 r,
     bytes32 s
   ) external returns (uint256) {
-    require(_badgeSpecOwners[tokenURI] != 0, "claimBadge: unauthorized");
-
     bytes memory signature = abi.encodePacked(r, s, v);
 
     uint256 tokenId = _badges.mintWithPermission(fromAddress, tokenURI, signature);
 
-    emit BadgeMinted(msg.sender, tokenId, tokenURI);
-
     return tokenId;
   }
 }
-
-
-/*
-ROMP as spec
-  - mint ROMP Token to Spec owner with badge spec as URI
-  - Validate fromAddress being owner of supplied tokenId
-  - Looking up URI for tokenId and then mint badge
-  Pros
-    -
-  Cons
-    - If ROMP is transferred after voucher and before claiming, mint will fail. New issuer must reissue vouchers
-
-ROMP as DAO
-  - set tokenURI as daoURI with EIP4824, pointing to otterspace API
-  - dao owner is also badge issuer (for now)
-  -
-  Pros
-  Cons
-    - how to create authorization?
-*/
--- src/BadgeVendor.t.sol
+++ src/BadgeVendor.t.sol
@@ -2,29 +2,12 @@
 pragma solidity ^0.8.6;
 
 import "forge-std/Test.sol";
-import "forge-std/console.sol";
 import {ERC4973Permit} from "ERC4973/ERC4973Permit.sol";
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import {BadgeVendor} from "./BadgeVendor.sol";
 import {Badges} from "./Badges.sol";
 
-string constant rompNFTBaseURI = "https://nftapi.com/";
-
-contract MockRompNFT is ERC721 {
-  constructor() ERC721("name", "symbol") {
-  }
-
-  function _baseURI() internal pure override returns (string memory) {
-    return rompNFTBaseURI;
-  }
-
-  function mint(address to, uint256 tokenId) public {
-    return super._mint(to, tokenId);
-  }
-}
-
-contract MockVendor is ERC4973Permit {
+contract MockBadges is ERC4973Permit {
   constructor() ERC4973Permit("name", "symbol", "version") {}
 
   function getHash(
@@ -32,7 +15,7 @@ contract MockVendor is ERC4973Permit {
     address to,
     string calldata tokenURI
   ) external view returns (bytes32) {
-    return super._getHash(from, to, tokenURI);
+    return _getHash(from, to, tokenURI);
   }
 }
 
@@ -40,17 +23,11 @@ contract MockVendor is ERC4973Permit {
 contract BadgeVendorTest is Test {
   Badges badges;
   BadgeVendor bv;
-
-  MockRompNFT romp;
-  MockVendor mv;
-
-  string name = "some name";
-  uint256 rompTokenId = 100;
-  string specUri = "ipfs://spec_uri";
+  MockBadges mb;
 
   // predetermined address for pregenerated test claim code signing
-  address exAddress = 0xb07dAd0000000000000000000000000000000001;
-	BadgeVendor target = BadgeVendor(exAddress);
+  // address exAddress = 0xb07dAd0000000000000000000000000000000001;
+	// BadgeVendor target = BadgeVendor(exAddress);
 
   address fromAddress = 0x0f6A79A579658E401E0B81c6dde1F2cd51d97176;
   uint256 fromPrivateKey = 0xad54bdeade5537fb0a553190159783e45d02d316a992db05cbed606d3ca36b39;
@@ -59,35 +36,23 @@ contract BadgeVendorTest is Test {
   uint256 toPrivateKey = 0x55b7b79aa0a71a634d00343ecb270adc0105d11566c7fcafa9381272d8d26554;
 
   function setUp() public {
-    romp = new MockRompNFT();
     badges = new Badges("name", "symbol", "version");
-    bv = new BadgeVendor(address(romp), address(badges));
-    mv = new MockVendor();
+    bv = new BadgeVendor(address(badges));
 
-    romp.mint(fromAddress, rompTokenId);
+    mb = new MockBadges();
 
-		vm.etch(exAddress, address(bv).code);
-  }
-
-  function testCreateBadgeSpec() public {
-    // vm.expectEmit(true, false, false, false);
-    vm.prank(fromAddress);
-    bv.createBadgeSpec(rompTokenId, specUri);
-    assertEq(bv.ownerOfSpec(specUri), rompTokenId);
+		// vm.etch(exAddress, address(bv).code);
   }
 
   function testClaimBadge() public {
-    vm.prank(fromAddress);
-    bv.createBadgeSpec(rompTokenId, specUri);
-    console.log(bv.ownerOfSpec(specUri));
-
-    bytes32 hash = mv.getHash(fromAddress, toAddress, specUri);
+    string memory tokenURI = "https://some-token-uri.com";
+    bytes32 hash = mb.getHash(fromAddress, toAddress, tokenURI);
     (uint8 v, bytes32 r, bytes32 s) = vm.sign(fromPrivateKey, hash);
 
     vm.prank(toAddress);
 		uint256 tokenId = bv.claimBadge(
       fromAddress,
-      specUri,
+      tokenURI,
       v,
       r,
       s
