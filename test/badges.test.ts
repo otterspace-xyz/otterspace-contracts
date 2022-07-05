f1055fa9d2b974d2029ffefe450e1aeae14288b3
George Pickett <gpickett00@gmail.com>
2022-07-05 15:59:27 +0200

    Remove package.lock.json and add yarn.lock. Add basic test

--- test/badges.test.ts
+++ test/badges.test.ts
@@ -1,20 +1,40 @@
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
+import { Badges } from '../typechain-types'
+import { Wallet } from 'ethers'
+import { waffle } from 'hardhat'
 
 describe('Badges', function () {
-    it('Should return then name of the badge', async function () {
+    let badgesContract: Badges
+
+    beforeEach(async () => {
         const Badges = await ethers.getContractFactory('Badges')
-        const badges = await Badges.deploy('Otter', 'OTTR', '1.0.0')
-        await badges.deployed()
-        expect(await badges.name()).to.equal('Otter')
+        badgesContract = await Badges.deploy('Otter', 'OTTR', '1.0.0')
+    })
+
+    it('Should return the name of the badge', async function () {
+        await badgesContract.deployed()
+        expect(await badgesContract.name()).to.equal('Otter')
+    })
+
+    it("should revert with message 'mintWithPermission: invalid signature', when given a bad signature", async () => {
+        await badgesContract.deployed()
+        const provider = waffle.provider
+        const sig = {
+            v: 28,
+            r: 'junk r value',
+            s: 'junk s value',
+            compact: 'jumk conpact value',
+            yParityAndS: 'junk parity value',
+            _vs: 'blah',
+        }
+        const sigAsBytes = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(JSON.stringify(sig)))
+        const issuerWallet = new Wallet('5f9a26937a48e1d6659c13f2115159886318545a405b33e9658e8188609cd80b', provider)
+        await expect(
+            badgesContract.mintWithPermission(issuerWallet.address, 'https://someURI.com', sigAsBytes)
+        ).to.be.revertedWith('mintWithPermission: invalid signature')
     })
 })
-
-// deploy the contract
-
-// pass contract arguments correctly
-
-// mint withn unauthorized signatures fails
 
 // mint with authorized signatures succeeds
 
