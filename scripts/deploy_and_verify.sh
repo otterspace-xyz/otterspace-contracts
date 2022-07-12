64a322447dddec6aebd665c38c91f11720159b3f
George Pickett <gpickett00@gmail.com>
2022-07-12 15:35:06 +0200

    Add new .env.rinkeby file for deployment

--- scripts/deploy_and_verify.sh
+++ scripts/deploy_and_verify.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-source .env
-forge create src/$CONTRACT_NAME.sol:$CONTRACT_NAME --constructor-args $CONSTRUCTOR_ARGS --private-key $PRIVATE_KEY --verify --rpc-url $ETH_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
+source .env.rinkeby
+forge create src/Badges.sol:Badges --constructor-args $BADGES_NAME $BADGES_SYMBOL $BADGES_VERSION --private-key $PRIVATE_KEY --verify --rpc-url $ETH_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
