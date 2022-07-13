27a4154f9ecb4e98acdeecf3d462388a1df94b7e
George Pickett <gpickett00@gmail.com>
2022-07-13 15:29:54 +0200

    Added more complete tests for RAFT

--- test/raft.test.ts
+++ test/raft.test.ts
@@ -1,21 +1,27 @@
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
 import { Raft } from '../typechain-types'
-import { Wallet } from 'ethers'
+import { Event, Wallet } from 'ethers'
 import { waffle } from 'hardhat'
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
 
 describe('Raft', async function () {
     let raftContract: Raft
-    const name = 'Raft NFT'
+    const name = 'Raft'
     const symbol = 'RAFT'
     const chainId = 31337
     const tokenURI = 'blah'
 
-    it('should deploy the contract with the right params', async function () {
+    async function deployContractFixture() {
         const Raft = await ethers.getContractFactory('Raft')
-        const [owner] = await ethers.getSigners()
+        const [owner, addr1] = await ethers.getSigners()
         const raftContract = await Raft.deploy(owner.address, name, symbol)
         await raftContract.deployed()
+        return { Raft, raftContract, owner, addr1 }
+    }
+
+    it('should deploy the contract with the right params', async function () {
+        const { raftContract } = await loadFixture(deployContractFixture)
         const deployedContractName = await raftContract.name()
         const deployedSymbolName = await raftContract.symbol()
         expect(deployedContractName).to.equal(name)
@@ -27,62 +33,52 @@ describe('Raft', async function () {
         expect(deployedToChainId).to.equal(chainId)
     })
 
-    it('owner should be able to mint an NFT when minting is pause', async function () {
-        const Raft = await ethers.getContractFactory('Raft')
-        const [owner] = await ethers.getSigners()
-        const raftContract = await Raft.deploy(owner.address, name, symbol)
-        await raftContract.deployed()
-
-        const [, recipient] = await ethers.getSigners()
-
-        const recipientAddress = recipient.address
+    it('should allow owner to mint when minting is paused', async function () {
+        const { raftContract, addr1 } = await loadFixture(deployContractFixture)
+        const recipientAddress = addr1.address
         const recipientBalance = await raftContract.balanceOf(recipientAddress)
 
         await raftContract.mint(recipientAddress, tokenURI)
         const recipientBalanceAfter = await raftContract.balanceOf(recipientAddress)
-
         expect(recipientBalanceAfter).to.equal(recipientBalance.add(1))
     })
 
     it('should prevent non-owner from minting when minting is paused', async function () {
-        const Raft = await ethers.getContractFactory('Raft')
-        const [owner, nonOwner] = await ethers.getSigners()
-        const raftContract = await Raft.deploy(owner.address, name, symbol)
-        await raftContract.deployed()
-        const nonOwnerAddress = nonOwner.address
-        const tx = await raftContract.mint(nonOwnerAddress, tokenURI)
-        const balance = await raftContract.balanceOf(nonOwnerAddress)
-        console.log('🚀 ~ balance', balance)
+        const { raftContract, addr1 } = await loadFixture(deployContractFixture)
+        const isPaused = await raftContract.paused()
+        expect(isPaused).to.equal(true)
 
-        expect(tx).to.be.revertedWith('mint: unauthorized to mint')
+        await expect(raftContract.connect(addr1).mint(addr1.address, tokenURI)).to.be.revertedWith(
+            'mint: unauthorized to mint'
+        )
     })
 
-    // should fetch then tokenURI after minting
+    it('should should allow non-owner to mint when minting is unpaused', async function () {
+        const { raftContract, addr1 } = await loadFixture(deployContractFixture)
+
+        await raftContract.unpause()
+        const isPaused = await raftContract.paused()
+        expect(isPaused).to.equal(false)
+
+        await raftContract.connect(addr1).mint(addr1.address, tokenURI)
+        expect(await raftContract.balanceOf(addr1.address)).to.equal(1)
+    })
+
     it('should fetch then tokenURI after minting', async function () {
-        const Raft = await ethers.getContractFactory('Raft')
-        const [owner] = await ethers.getSigners()
-        const raftContract = await Raft.deploy(owner.address, name, symbol)
-        await raftContract.deployed()
-        const [, recipient] = await ethers.getSigners()
-        const recipientAddress = recipient.address
-        const tokenURI = 'blah'
-        await raftContract.mint(recipientAddress, tokenURI)
-        const fetchedTokenURI = await raftContract.tokenURI(recipientAddress)
-        expect(fetchedTokenURI).to.equal(tokenURI)
+        const { raftContract, addr1 } = await loadFixture(deployContractFixture)
+
+        const tx = await raftContract.mint(addr1.address, tokenURI)
+        const txReceipt = await tx.wait()
+        const [transferEvent] = txReceipt.events!
+        const { tokenId: rawTokenId } = transferEvent.args!
+        const tokenId = ethers.BigNumber.from(rawTokenId).toNumber()
+
+        const fetchedTokenURI = await raftContract.tokenURI(tokenId)
+        expect(tokenURI).to.equal(fetchedTokenURI)
     })
 
-    // should not allow minting when non-owner calls contract when minting is paused
-
-    // should allow owner to mint when minting is paused
-
-    // should not allow non-owner to call pause
     it('should not allow non-owner to call pause', async function () {
-        const Raft = await ethers.getContractFactory('Raft')
-        const [, nonOwner] = await ethers.getSigners()
-        const nonOwnerAddress = nonOwner.address
-        const raftContract = await Raft.deploy(nonOwnerAddress, name, symbol)
-        await raftContract.deployed()
-        const tx = await raftContract.pause()
-        expect(tx).to.be.revertedWith('pause: unauthorized to pause')
+        const { raftContract, addr1 } = await loadFixture(deployContractFixture)
+        await expect(raftContract.connect(addr1).pause()).to.be.revertedWith('Ownable: caller is not the owner')
     })
 })
