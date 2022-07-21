7256af3817dfd1e2fdb05e842ccb990acba814ee
Rahul Rumalla <rahulrumalla@gmail.com>
2022-07-21 16:35:14 +0200

    Reorganizing

--- test/badges.test.ts
+++ test/badges.test.ts
@@ -7,65 +7,65 @@ import { splitSignature, _TypedDataEncoder } from 'ethers/lib/utils'
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
 import { PromiseOrValue } from '../typechain-types/common'
 
+const testSpecUri = 'test spec uri'
+const name = 'Otter'
+const symbol = 'OTTR'
+const version = '1'
+const chainId = 31337
+const specUri = 'some spec uri'
+const incorrectUri = 'https://some-incorrect-uri.com'
+
+const createSpec = async (uri: PromiseOrValue<string>, badgesContract: Badges, raftTokenId: any) => {
+  const specTx = await badgesContract.createSpecAsRaftOwner(uri, raftTokenId)
+  await specTx.wait()
+}
+
+async function deployContractFixture() {
+  const badges = await ethers.getContractFactory('Badges')
+  const [owner, issuer, claimant, claimant2] = await ethers.getSigners()
+
+  const badgesContract = await badges.deploy(name, symbol, version)
+  await badgesContract.deployed()
+
+  const raft = await ethers.getContractFactory('Raft')
+  const raftContract = await raft.deploy(owner.address, name, symbol)
+  await raftContract.deployed()
+
+  const tx = await raftContract.mint(issuer.address, specUri)
+  const txReceipt = await tx.wait()
+
+  const [transferEvent] = txReceipt.events!
+  const { tokenId: rawTokenId } = transferEvent.args!
+  const raftTokenId = ethers.BigNumber.from(rawTokenId).toNumber()
+  const tokenOwner = await raftContract.ownerOf(raftTokenId)
+  expect(tokenOwner).to.equal(issuer.address)
+
+  await createSpec(specUri, badgesContract, raftTokenId)
+
+  const typedData = {
+    domain: {
+      name: name,
+      version: version,
+      chainId,
+      verifyingContract: badgesContract.address,
+    },
+    types: {
+      Agreement: [
+        { name: 'active', type: 'address' },
+        { name: 'passive', type: 'address' },
+        { name: 'tokenURI', type: 'string' },
+      ],
+    },
+    value: {
+      active: claimant.address,
+      passive: issuer.address,
+      tokenURI: specUri,
+    },
+  }
+  return { badges, badgesContract, owner, issuer, claimant, claimant2, typedData, raftTokenId }
+}
+
 describe('Badges', async function () {
-  const name = 'Otter'
-  const symbol = 'OTTR'
-  const version = '1'
-  const chainId = 31337
-  const specUri = 'some spec uri'
-  const testSpecUri = 'test spec uri'
-  const incorrectUri = 'https://some-incorrect-uri.com'
-
-  const createSpec = async (uri: PromiseOrValue<string>, badgesContract: Badges, raftTokenId: any) => {
-    const specTx = await badgesContract.createSpecAsRaftOwner(uri, raftTokenId)
-    await specTx.wait()
-  }
-
-  async function deployContractFixture() {
-    const badges = await ethers.getContractFactory('Badges')
-    const [owner, issuer, claimant, claimant2] = await ethers.getSigners()
-
-    const badgesContract = await badges.deploy(name, symbol, version)
-    await badgesContract.deployed()
-
-    const raft = await ethers.getContractFactory('Raft')
-    const raftContract = await raft.deploy(owner.address, name, symbol)
-    await raftContract.deployed()
-
-    const tx = await raftContract.mint(issuer.address, specUri)
-    const txReceipt = await tx.wait()
-
-    const [transferEvent] = txReceipt.events!
-    const { tokenId: rawTokenId } = transferEvent.args!
-    const raftTokenId = ethers.BigNumber.from(rawTokenId).toNumber()
-    const tokenOwner = await raftContract.ownerOf(raftTokenId)
-    expect(tokenOwner).to.equal(issuer.address)
-
-    await createSpec(specUri, badgesContract, raftTokenId)
-
-    const typedData = {
-      domain: {
-        name: name,
-        version: version,
-        chainId,
-        verifyingContract: badgesContract.address,
-      },
-      types: {
-        Agreement: [
-          { name: 'active', type: 'address' },
-          { name: 'passive', type: 'address' },
-          { name: 'tokenURI', type: 'string' },
-        ],
-      },
-      value: {
-        active: claimant.address,
-        passive: issuer.address,
-        tokenURI: specUri,
-      },
-    }
-    return { badges, badgesContract, owner, issuer, claimant, claimant2, typedData, raftTokenId }
-  }
-
   it('should deploy the contract with the right params', async function () {
     const { badgesContract } = await loadFixture(deployContractFixture)
 
@@ -173,6 +173,9 @@ describe('Badges', async function () {
     const uriOfToken = await badgesContract.tokenURI(tokenId)
     expect(uriOfToken).to.equal(typedData.value.tokenURI)
   })
+})
+
+describe('Badge Specs', () => {
   it('should register a spec successfully', async function () {
     const { badgesContract, raftTokenId } = await deployContractFixture()
     await createSpec(testSpecUri, badgesContract, raftTokenId)
