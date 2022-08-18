6061fb0282f112b55e469bb99e5e43f4f88d8f1a
George Pickett <gpickett00@gmail.com>
2022-08-18 15:08:14 +0200

    Remove old scripts

--- scripts/deploy_and_verify_raft.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# $1: the name of the .env file to use
-source $1
-forge create src/Raft.sol:Raft --constructor-args $RAFT_OWNER_ADDRESS $RAFT_NAME $RAFT_SYMBOL --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $ETH_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
