613734f3af9e482494ab9642f42407875d5d231d
George Pickett <gpickett00@gmail.com>
2022-10-05 11:43:52 -0400

    [DEV-595] - add .env templates and update hardhat config (#94)
    * fix conflicts from cherry-pick
    
    * Remove unnecessary scripts
    
    * Update gh action to reflect new .env var

--- scripts/deploy_and_verify_raft.sh
+++ scripts/deploy_and_verify_raft.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+
 # $1: the name of the .env file to use
 source $1
-forge create src/Raft.sol:Raft --constructor-args $RAFT_OWNER_ADDRESS $RAFT_NAME $RAFT_SYMBOL --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $ETH_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
+
+echo "NEXT_OWNER=$NEXT_OWNER"
+echo "RAFT_NAME=$RAFT_NAME"
+echo "RAFT_SYMBOL=$RAFT_SYMBOL"
+echo "DEPLOYER_PRIVATE_KEY=$DEPLOYER_PRIVATE_KEY"
+echo "GOERLI_RPC_URL=$GOERLI_RPC_URL"
+echo "ETHERSCAN_API_KEY=$ETHERSCAN_API_KEY"
+echo "OPTIMISTIC_ETHERSCAN_API_KEY=$OPTIMISTIC_ETHERSCAN_API_KEY"
+echo "OPTIMISM_RPC_URL=$OPTIMISM_RPC_URL"
+
+forge create src/Raft.sol:Raft --constructor-args $NEXT_OWNER $RAFT_NAME $RAFT_SYMBOL --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $ETH_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
