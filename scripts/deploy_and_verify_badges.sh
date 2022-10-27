e95df52ac57e1e4208efb5d96769c55974a5d35d
grp06 <gpickett00@gmail.com>
2022-10-27 10:46:39 -0300

    Update deployment script to handle both goerli and optimism

--- scripts/deploy_and_verify_badges.sh
+++ scripts/deploy_and_verify_badges.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# call this script like this:
+# ./scripts/deploy_and_verify_badges.sh .env.badges goerli
+
 # $1: the name of the .env file to use
 source $1
 
@@ -14,5 +17,11 @@ echo "ETHERSCAN_API_KEY=$ETHERSCAN_API_KEY"
 echo "OPTIMISTIC_ETHERSCAN_API_KEY=$OPTIMISTIC_ETHERSCAN_API_KEY"
 echo "OPTIMISM_RPC_URL=$OPTIMISM_RPC_URL"
 
-forge create src/Badges.sol:Badges --constructor-args $BADGES_NAME $BADGES_SYMBOL $BADGES_VERSION $RAFT_ADDRESS $NEXT_OWNER --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $GOERLI_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
+if [ $2 == "goerli" ]; then
+  echo "Deploying to Goerli"
+  forge create src/Badges.sol:Badges --constructor-args $BADGES_NAME $BADGES_SYMBOL $BADGES_VERSION $RAFT_ADDRESS $NEXT_OWNER --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $GOERLI_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
+elif [ $2 == "optimism" ]; then
+  echo "Deploying to Optimism"
+  forge create src/Badges.sol:Badges --constructor-args $BADGES_NAME $BADGES_SYMBOL $BADGES_VERSION $RAFT_ADDRESS $NEXT_OWNER --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $OPTIMISM_RPC_URL --etherscan-api-key $OPTIMISTIC_ETHERSCAN_API_KEY
+fi
 
