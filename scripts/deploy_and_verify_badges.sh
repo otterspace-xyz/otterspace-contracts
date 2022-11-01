f51a814008b5728637139735eb5d9f1689b7b318
George Pickett <gpickett00@gmail.com>
2022-11-01 11:41:48 -0400

    Remove constructor args from deployment script (#102)
    * Remove constructor args from deployment script
    
    * Remove init variables from .env.example since theyre not needed to deploy implementation contract

--- scripts/deploy_and_verify_badges.sh
+++ scripts/deploy_and_verify_badges.sh
@@ -6,11 +6,6 @@
 # $1: the name of the .env file to use
 source $1
 
-echo "BADGES_NAME=$BADGES_NAME"
-echo "BADGES_SYMBOL=$BADGES_SYMBOL"
-echo "BADGES_VERSION=$BADGES_VERSION"
-echo "RAFT_ADDRESS=$RAFT_ADDRESS"
-echo "NEXT_OWNER=$NEXT_OWNER"
 echo "DEPLOYER_PRIVATE_KEY=$DEPLOYER_PRIVATE_KEY"
 echo "GOERLI_RPC_URL=$GOERLI_RPC_URL"
 echo "ETHERSCAN_API_KEY=$ETHERSCAN_API_KEY"
@@ -19,9 +14,9 @@ echo "OPTIMISM_RPC_URL=$OPTIMISM_RPC_URL"
 
 if [ $2 == "goerli" ]; then
   echo "Deploying to Goerli"
-  forge create src/Badges.sol:Badges --constructor-args $BADGES_NAME $BADGES_SYMBOL $BADGES_VERSION $RAFT_ADDRESS $NEXT_OWNER --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $GOERLI_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
+  forge create src/Badges.sol:Badges --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $GOERLI_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
 elif [ $2 == "optimism" ]; then
   echo "Deploying to Optimism"
-  forge create src/Badges.sol:Badges --constructor-args $BADGES_NAME $BADGES_SYMBOL $BADGES_VERSION $RAFT_ADDRESS $NEXT_OWNER --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $OPTIMISM_RPC_URL --etherscan-api-key $OPTIMISTIC_ETHERSCAN_API_KEY
+  forge create src/Badges.sol:Badges --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $OPTIMISM_RPC_URL --etherscan-api-key $OPTIMISTIC_ETHERSCAN_API_KEY
 fi
 
