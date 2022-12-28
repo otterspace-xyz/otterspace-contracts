02c0ef3f94f5b002f91b3bce33c038c0f75aafc6
grp06 <gpickett00@gmail.com>
2022-12-28 12:40:43 -0300

    actions test

--- scripts/deploy_and_verify_implementation.sh
+++ scripts/deploy_and_verify_implementation.sh
@@ -3,21 +3,21 @@
 # call this script like this:
 # ./scripts/deploy_and_verify_badges.sh .env.badges goerli
 
+# we took this out since we dont need to pass in an .env file since we're running it from GH actions
 # $1: the name of the .env file to use
-source $1
 
-echo "Deploying $2 to $3"
+echo "1: $1"
+echo "2: $2"
 echo "DEPLOYER_PRIVATE_KEY=$DEPLOYER_PRIVATE_KEY"
 
-if [ $3 == "goerli" ]; then
-  echo " "  
+if [ "$2" == "goerli" ]; then
+  echo " "
   echo "GOERLI_RPC_URL=$GOERLI_RPC_URL"
   echo "ETHERSCAN_API_KEY=$ETHERSCAN_API_KEY"
-  forge create src/$2.sol:$2 --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $GOERLI_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
-elif [ $3 == "optimism" ]; then
+  # forge create src/$1.sol:$1 --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $GOERLI_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
+elif [ "$2" == "optimism" ]; then
   echo " "
   echo "OPTIMISTIC_ETHERSCAN_API_KEY=$OPTIMISTIC_ETHERSCAN_API_KEY"
   echo "OPTIMISM_RPC_URL=$OPTIMISM_RPC_URL"
-  forge create src/$2.sol:$2 --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $OPTIMISM_RPC_URL --etherscan-api-key $OPTIMISTIC_ETHERSCAN_API_KEY
+  forge create src/$1.sol:$1 --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $OPTIMISM_RPC_URL --etherscan-api-key $OPTIMISTIC_ETHERSCAN_API_KEY
 fi
-
