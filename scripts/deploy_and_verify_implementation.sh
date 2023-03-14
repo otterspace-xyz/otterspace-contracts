b71e44f984eb3d72ddd2e5de5d2f7f97163927dc
grp06 <gpickett00@gmail.com>
2023-03-14 14:26:08 -0300

    Refactor github action to loop through networks

--- scripts/deploy_and_verify_implementation.sh
+++ scripts/deploy_and_verify_implementation.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
 
 # call this script like this:
-# ./scripts/deploy_and_verify_badges.sh .env.badges goerli
+# ./scripts/deploy_and_verify_badges.sh .env.implementation goerli
 
 # $1: the name of the .env file to use
 source $1
 
 echo "Deploying $2 to $3"
-echo "DEPLOYER_PRIVATE_KEY=$DEPLOYER_PRIVATE_KEY"
 
 if [ $3 == "goerli" ]; then
-  echo " "  
+  echo " "
   echo "GOERLI_RPC_URL=$GOERLI_RPC_URL"
   echo "ETHERSCAN_API_KEY=$ETHERSCAN_API_KEY"
   forge create src/$2.sol:$2 --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $GOERLI_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
+elif [ $3 == "optimism-goerli" ]; then
+  echo " "
+  echo "OPTIMISM_GOERLI_RPC_URL=$OPTIMISM_GOERLI_RPC_URL"
+  echo "OPTIMISTIC_ETHERSCAN_API_KEY=$OPTIMISTIC_ETHERSCAN_API_KEY"
+  forge create src/$2.sol:$2 --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $OPTIMISM_GOERLI_RPC_URL --etherscan-api-key $OPTIMISTIC_ETHERSCAN_API_KEY
 elif [ $3 == "optimism" ]; then
   echo " "
   echo "OPTIMISTIC_ETHERSCAN_API_KEY=$OPTIMISTIC_ETHERSCAN_API_KEY"
   echo "OPTIMISM_RPC_URL=$OPTIMISM_RPC_URL"
   forge create src/$2.sol:$2 --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $OPTIMISM_RPC_URL --etherscan-api-key $OPTIMISTIC_ETHERSCAN_API_KEY
 fi
-
