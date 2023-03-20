0b465356269bc036c99efc22ecfe474d34835447
grp06 <gpickett00@gmail.com>
2023-03-20 14:30:14 -0300

    update dev-merge to deploy to goerli, optimism-goerli and optimism

--- scripts/deploy_and_verify_implementation.sh
+++ scripts/deploy_and_verify_implementation.sh
@@ -12,15 +12,15 @@ if [ $3 == "goerli" ]; then
   echo " "
   echo "GOERLI_RPC_URL=$GOERLI_RPC_URL"
   echo "ETHERSCAN_API_KEY=$ETHERSCAN_API_KEY"
-  forge create src/$2.sol:$2 --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $GOERLI_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY
+  forge create src/$2.sol:$2 --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $GOERLI_RPC_URL --etherscan-api-key $ETHERSCAN_API_KEY --gas-limit 4000000
 elif [ $3 == "optimism-goerli" ]; then
   echo " "
   echo "OPTIMISM_GOERLI_RPC_URL=$OPTIMISM_GOERLI_RPC_URL"
   echo "OPTIMISTIC_ETHERSCAN_API_KEY=$OPTIMISTIC_ETHERSCAN_API_KEY"
-  forge create src/$2.sol:$2 --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $OPTIMISM_GOERLI_RPC_URL --etherscan-api-key $OPTIMISTIC_ETHERSCAN_API_KEY
+  forge create src/$2.sol:$2 --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $OPTIMISM_GOERLI_RPC_URL --etherscan-api-key $OPTIMISTIC_ETHERSCAN_API_KEY --gas-limit 4000000
 elif [ $3 == "optimism" ]; then
   echo " "
   echo "OPTIMISTIC_ETHERSCAN_API_KEY=$OPTIMISTIC_ETHERSCAN_API_KEY"
   echo "OPTIMISM_RPC_URL=$OPTIMISM_RPC_URL"
-  forge create src/$2.sol:$2 --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $OPTIMISM_RPC_URL --etherscan-api-key $OPTIMISTIC_ETHERSCAN_API_KEY
+  forge create src/$2.sol:$2 --private-key $DEPLOYER_PRIVATE_KEY --verify --rpc-url $OPTIMISM_RPC_URL --etherscan-api-key $OPTIMISTIC_ETHERSCAN_API_KEY --gas-limit 4000000
 fi
